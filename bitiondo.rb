6ccd217fec03fa8cefe37463177ebcad55e5c9ce
martinezfabiola <fabimartinezperez@gmail.com>
2017-10-17 15:35:11 -0400

    Expresiones Regulares (Falta Cadena de Caracteres y algunas de caracteres)

--- bitiondo.rb
+++ bitiondo.rb
@@ -34,37 +34,50 @@ class Lexer
 		@column = 1
 
 		@tokensdict = {
-			ignore: /\A#.+|\s+/,
-			int: /\Aint\b/,
-			bool: /\Abool\b/,
-			true: /\Atrue\b/,
-			false: /\Afalse\b/,
-			bits: /\Abits\b/,
-			plus: /\A\+/, #FIX
-			minus: /\A\+/, #FIX
-			mult: /\A\*/, #FIX
-			# Construir expresiones regulares para detectar operadores
-			bitsexpression: /\A0b[0-1]+/,
+			# Numbers:
+			numeric: /[0-9]+/,
+
+			# Reserved words:
 			beginbit: /\Abegin\b/,
 			endbit: /\Aend\b/,
-			input: /\Ainput\b/,
-			output: /\Aoutput\b/,
-			outputln: /\Aoutputln\b/,
-			#string: Hacer
 			ifcond: /\Aif\b/,
 			elsecond: /\Aelse\b/,
 			forcond: /\Afor\b/,
-			numeric: /[0-9]+/,
 			forbits: /\Aforbits\b/,
 			as: /\Aas\b/,
 			from: /\Afrom\b/,
 			going: /\Agoing\b/,
 			higher: /\Ahigher\b/,
 			lower: /\Alower\b/,
-			repeat: /\Arepeat\b/,
 			whilecond: /\Awhile\b/,
 			docond: /\Ado\b/,
+			repeat: /\Arepeat\b/,
+			input: /\Ainput\b/,
+			output: /\Aoutput\b/,
+			outputln: /\Aoutputln\b/,
+			true: /\Atrue\b/,
+			false: /\Afalse\b/,
+			
+			# Characters Chain
+			
+			# Simbols:
+			plus: /\A\+\b/, 
+			minus: /\A\-\b/, 
+			mult: /\A\*\b/, 
+			div: /\A\/\b/,
+
+			# Data Type:
+			int: /\Aint\b/,
+			bool: /\Abool\b/,
+			bits: /\Abits\b/,
+			bitsexpression: /\A0b[0-1]+/,
+
+			# Identifiers:
 			identifier: /\A[A-Za-z][A-Za-z0-9\_]*(?:\[[0-9]+\])?$/
+
+			# Character Unexpected
+			ignore: /\A#.+|\s+/,
+
 		}
 
 	end
